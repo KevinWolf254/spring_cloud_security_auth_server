00e48529f775e33b1b7fead34414ff8eaeed5571
Kanyi-PC <mbugua.kevinkanyi@gmail.com>
2018-08-30 05:46:38 +0300

    initial commit

---
+++ spring-cloud-security-auth-server-/src/main/java/main/SecurityConfig.java
@@ -0,0 +1,20 @@
+package main;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends GlobalAuthenticationConfigurerAdapter{
+
+	public void init(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+				.withUser("allen")
+				.password("pass")
+				.roles("USER")
+			.and()
+				.withUser("ben")
+				.password("pass1")
+				.roles("USER","ADMIN");
+	}
+}
